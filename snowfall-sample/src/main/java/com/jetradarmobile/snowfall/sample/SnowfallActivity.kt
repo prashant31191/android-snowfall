29d00ed18f6c31d306e9ce0262853704d86fa081
Prashant Chovatiya <prashant.chovatiya@eliteinfoworld.com>
2017-07-17 15:45:51 +0530

    first commit

--- snowfall-sample/src/main/java/com/jetradarmobile/snowfall/sample/SnowfallActivity.kt
+++ snowfall-sample/src/main/java/com/jetradarmobile/snowfall/sample/SnowfallActivity.kt
@@ -2,11 +2,21 @@ package com.jetradarmobile.snowfall.sample
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.jetradarmobile.snowfall.SnowfallView
 
 class SnowfallActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_snowfall)
+    val btnSetMaxSpeed = findViewById(R.id.btnSetMaxSpeed) as Button
+    val sfView = findViewById(R.id.sfView) as SnowfallView
+
+    btnSetMaxSpeed.setOnClickListener { view ->
+      Log.i("=click=","btn");
+
+    }
   }
 }
